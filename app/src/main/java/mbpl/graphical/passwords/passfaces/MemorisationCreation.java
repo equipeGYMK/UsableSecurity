3c568e3ef4a55897ec4f2a3b67e9a3c8f031bb5f
equipeGYMK <equipe.gymk@gmail.com>
2017-04-28 14:51:40 +0200

    javadoc PF memorisationCreation

--- app/src/main/java/mbpl/graphical/passwords/passfaces/MemorisationCreation.java
+++ app/src/main/java/mbpl/graphical/passwords/passfaces/MemorisationCreation.java
@@ -36,11 +36,9 @@ public class MemorisationCreation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_validation_creation);
 
-        // action bar
         setTitle("Passfaces Mémorisation");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        //récupérer les éléments
         buttonSubmit = (Button) findViewById(R.id.buttonTerminerValidationCreationPassFace);
         buttonNext = (Button) findViewById(R.id.buttonNextValidationCreation);
         buttonPrevious = (Button) findViewById(R.id.buttonPreviousValidationCreation);
@@ -49,62 +47,48 @@ public class MemorisationCreation extends AppCompatActivity {
         imageView = (ImageView) findViewById(R.id.imageViewValidation);
         imageView.bringToFront();
 
-        //mettre en place le texte
         textViewExplication.setText("Regardez attentivement cette personne.\n" +
                 "À qui pensez-vous qu'elle ressemble?\n" +
                 "Elle vous rappelle quelqu'un?");
 
-        //récupération mot de passe
         methodeManager = new MethodeManager(getApplicationContext());
         methodeManager.open();
         methode = methodeManager.getMethode(methode);
         trueMotDePasse = Tools.stringArrayToIntArray(methode.getMdp());
 
-        //Ajout des listeners. Achaque fois que l'on cliquera sur un bouton, on appellera la fonction traitementVerification avec le compteur modifié
         addListenerOnButton();
     }
 
-
-
+    /**
+     * Modifie l'image et verifie les changement qui doivent être apportés
+     */
     public void traitementVerification(){
-        //Création du bitmap
         Bitmap bmp;
         bmp = BitmapFactory.decodeResource(getResources(), getDrawableN(trueMotDePasse.get(compteur - 1)));
         bmp = Bitmap.createScaledBitmap(bmp, 256, 256, true);
         imageView.setImageBitmap(bmp);
 
-
-        //si l'image est agrandi et qu'on change d'image, il faut s'assurer que le texte soit le bon
         if (isFullScreen)
             textViewZoom.setText("Cliquez sur l'image pour l'agrandir");
 
-        //Gérer les boutons selon le compteur
         EnableBouton();
     }
-
-
-
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        //Mettre le texte
         textViewZoom.setText("Cliquez sur l'image pour l'agrandir");
-        //Première image au lancement
         Bitmap bmp;
         bmp = BitmapFactory.decodeResource(getResources(), getDrawableN(trueMotDePasse.get(0)));
         bmp = Bitmap.createScaledBitmap(bmp, 256, 256, true);
 
-        // On ajoute l'image à l'ImageView
         imageView = (ImageView) findViewById(R.id.imageViewValidation);
         imageView.setImageBitmap(bmp);
 
-        //laisser les boutons utiles
         buttonNext.setEnabled(true);
         buttonPrevious.setEnabled(false);
     }
-
 
     public void EnableBouton(){
 
@@ -123,15 +107,16 @@ public class MemorisationCreation extends AppCompatActivity {
     }
 
 
-    // get the selected dropdown list value
+    /**
+     * Initialise les listeners des boutons de l'interface
+     * Initialise le listener sur l'imageView
+     */
     public void addListenerOnButton() {
 
         buttonNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //On incrémente le compteur
                 compteur++;
-                //appel de la fonction. callback
                 traitementVerification();
                 isFullScreen = false;
             }
@@ -140,9 +125,7 @@ public class MemorisationCreation extends AppCompatActivity {
         buttonPrevious.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               //On décrémente le compteur
                 compteur--;
-                //appel de la fonction. callback
                 traitementVerification();
                 isFullScreen = false;
             }
@@ -165,26 +148,22 @@ public class MemorisationCreation extends AppCompatActivity {
 
                 if (!isFullScreen)
                 {
-                    //agrandissement de l'image
                     Bitmap bmp;
                     bmp = BitmapFactory.decodeResource(getResources(), getDrawableN(trueMotDePasse.get(compteur - 1)));
                     bmp = Bitmap.createScaledBitmap(bmp, 512, 512, true);
                     imageView.setImageBitmap(bmp);
                     isFullScreen = true;
 
-                    //texte
                     textViewZoom.setText("Cliquez sur l'image pour la rétrécir");
                 }
                 else
                 {
-                    //agrandissement de l'image
                     Bitmap bmp;
                     bmp = BitmapFactory.decodeResource(getResources(), getDrawableN(trueMotDePasse.get(compteur - 1)));
                     bmp = Bitmap.createScaledBitmap(bmp, 256, 256, true);
                     imageView.setImageBitmap(bmp);
                     isFullScreen = false;
 
-                    //texte
                     textViewZoom.setText("Cliquez sur l'image pour l'agrandir");
                 }
             }
@@ -202,7 +181,11 @@ public class MemorisationCreation extends AppCompatActivity {
         }
     }
 
-
+    /**
+     * Retourne l'image contenu dans la base de données à l'indice donnée
+     * @param n
+     * return l'image à l'indice n
+     */
     protected int getDrawableN(int n) {
         return getResources().getIdentifier("visage_" + n, "drawable", getPackageName());
     }
